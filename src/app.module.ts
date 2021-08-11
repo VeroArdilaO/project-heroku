fc17277cfb9fb5166a8dbb2d1a88d0aa749cc8c8
Veronica <veronica.ardilao@outlook.es>
2021-08-11 17:08:41 -0500

    i created the html static and the documentation for the health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -5,18 +5,18 @@ import * as os from 'os';
 
 @Controller('_health')
 export class HealthController {
-    @Get()
-    get(@Res() response: Response): Response {
-        const hostname = os.hostname();
-        const startTime = new Date(Date.now() - process.uptime()).toISOString();
-        return response.status(HttpStatus.OK).send({
-            available: true,
-            //app: name,
-            uptime: process.uptime(),
-            serverId: `${hostname} ${process.pid}`,
-            hostname,
-            startTime,
-            //version,
-        });
-    }
+  @Get()
+  get(@Res() response: Response): Response {
+    const hostname = os.hostname();
+    const startTime = new Date(Date.now() - process.uptime()).toISOString();
+    return response.status(HttpStatus.OK).send({
+      available: true,
+      //app: name,
+      uptime: process.uptime(),
+      serverId: `${hostname} ${process.pid}`,
+      hostname,
+      startTime,
+      //version,
+    });
+  }
 }
